b3e2bfc51939d8d1063812011193610825593537
Ashutosh raj <91281709+raj075512@users.noreply.github.com>
2025-01-17 07:56:58 +0530

    Update Orderbook.cpp

--- Orderbook.cpp
+++ Orderbook.cpp
@@ -4,55 +4,7 @@
 #include <chrono>
 #include <ctime>
 
-void Orderbook::PruneGoodForDayOrders()
-{
-	using namespace std::chrono;
-	const auto end = hours(16);
 
-	while (true)
-	{
-		const auto now = system_clock::now();
-		const auto now_c = system_clock::to_time_t(now);
-		std::tm now_parts;
-		localtime_s(&now_parts, &now_c);
-
-		if (now_parts.tm_hour >= end.count())
-			now_parts.tm_mday += 1;
-
-		now_parts.tm_hour = end.count();
-		now_parts.tm_min = 0;
-		now_parts.tm_sec = 0;
-
-		auto next = system_clock::from_time_t(mktime(&now_parts));
-		auto till = next - now + milliseconds(100);
-
-		{
-			std::unique_lock ordersLock{ ordersMutex_ };
-
-			if (shutdown_.load(std::memory_order_acquire) ||
-				shutdownConditionVariable_.wait_for(ordersLock, till) == std::cv_status::no_timeout)
-				return;
-		}
-
-		OrderIds orderIds;
-
-		{
-			std::scoped_lock ordersLock{ ordersMutex_ };
-
-			for (const auto& [_, entry] : orders_)
-			{
-				const auto& [order, _] = entry;
-
-				if (order->GetOrderType() != OrderType::GoodForDay)
-					continue;
-
-				orderIds.push_back(order->GetOrderId());
-			}
-		}
-
-		CancelOrders(orderIds);
-	}
-}
 
 void Orderbook::CancelOrders(OrderIds orderIds)
 {
